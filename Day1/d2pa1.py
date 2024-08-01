d8cd82f12a2484f89172a87a1df90a57df308e97
Archana802 <archanarajan73@gmail.com>
2024-08-01 08:19:23 +0530

    Add files via upload
    Day2 Program assignment

---
+++ Day1/d2pa1.py
@@ -0,0 +1,12 @@
+#Find sum of Even placed digits in a number
+
+input_number = int(input('Enter a number to find sum of even digits in it: '))
+digits = str(input_number)
+
+
+total_sum = 0
+
+for i in range(1, len(digits), 2): 
+        total_sum +=int(digits[i])
+    
+print('sum of Even placed digits in a number is ' + str(total_sum))
