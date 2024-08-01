879b6f6cf8f5f057748e6d0324279e18cf7b5658
Archana802 <archanarajan73@gmail.com>
2024-08-01 10:03:31 +0530

    Update d2pa6.py
    Done

--- Day1/d2pa6.py
+++ Day1/d2pa6.py
@@ -1,1 +1,13 @@
 #6. Print the Pascals triangle of n lines
+
+from math import factorial
+
+
+number = int(input('enter the number of lines:'))
+
+for i in range(number):
+  for j in range(number-i+1):
+    print('')
+  for j in range(i+1):
+    print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+  print()
